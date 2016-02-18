92e3c7aea1945257a169bac640b981d0a031bd24
Chris Gibson <cigibson33@gmail.com>
2016-02-18 16:57:12 -0600

    first commit project basecode

---
+++ app.js
@@ -0,0 +1,12 @@
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('port', process.env.PORT || 5000);
+
+app.get('/*', function(req, res) {
+    console.log("Here is the request: " , req.params);
+    var file = req.params[0] || '/views/index.html';
+    res.sendFile(path.join(__dirname, './public/', file));
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Server is ready on port ' + app.get('port'));
+});
